0819956ee19ed62f25e0055a5943f852d0a4f37c
Carlo Palinckx <carlo@mijnwebwinkel.nl>
2018-03-05 21:40:16 +0100

    Added typeguard support

--- src/factory/factory.test.ts
+++ src/factory/factory.test.ts
@@ -2,10 +2,8 @@ import factory from './factory';
 import evaluate from '../evaluate/evaluate';
 
 jest.mock('../evaluate/evaluate', () => {
-    return jest.fn().mockImplementation(
-        () => {
-            return jest.fn().mockImplementation((subject:any) => subject);
-        },
+    return jest.fn().mockReturnValue(
+        jest.fn().mockImplementation((subject:any) => subject),
     );
 });
 
@@ -15,13 +13,51 @@ interface Product {
 }
 
 describe('a curried factory function that allows for easy setting of defaults', () => {
-    const defaults:any = { foo: null, bar: 'bar' };
+    const defaults:Product = { foo: 'foo', bar: 'bar' };
 
     it('curries into a product', () => {
-        const constructionData = { foo: 'foo', bar: 'foo', hurr:'durr' };
-        const product = factory<Product>(defaults)(constructionData);
+        const productFactory = factory(defaults)();
+        const product = productFactory({ foo: 'bar', bar: 'foo' });
 
-        expect(evaluate).toHaveBeenCalledTimes(1);
-        expect(product).toEqual(constructionData);
+        expect(product).toEqual({ foo: 'bar', bar: 'foo' });
+    });
+
+    it('throws an exception on failing evaluation', () => {
+        (evaluate as jest.Mock).mockReturnValueOnce(() => false);
+
+        /*
+         * We cast our constructionData as any to skip compiler checks
+         * and fake a scenario where factory input is uncertain or untyped.
+         */
+        const constructionData:any = { foo: '', bar: 19.12 };
+        const productFactory = factory(defaults)();
+
+        const fn = ():void => {
+            productFactory(constructionData);
+        };
+
+        expect(fn).toThrow('Invalid construction, evaluation didn\'t pass');
+    });
+
+    it('throws an exception on failing type guard', () => {
+        (evaluate as jest.Mock).mockReturnValueOnce(() => true);
+
+        /*
+         * the typeGuard is used to provide strict construction rules for a product.
+         * It should take the product as a argument and should have a boolean as
+         * return value.
+         */
+        const typeGuard = (product:Product):boolean => {
+            return product.bar.length > 3;
+        };
+
+        const failingData:Product = { foo: '', bar: '' };
+        const productFactory = factory(defaults)(typeGuard);
+
+        const fn = ():void => {
+            productFactory(failingData);
+        };
+
+        expect(fn).toThrow('Invalid construction, type guard didn\'t pass');
     });
 });
